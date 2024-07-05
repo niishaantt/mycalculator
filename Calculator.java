26234bb08c87c8b845ff85e7660cf9dd85c71d4e
Nishant Yadav <nishantyadav1732@gmail.com>
2024-07-05 13:09:47 +0530

    First Commit, added calculators and main classes in repositories

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator{
+	
+	public void add(int a, int b){
+		System.out.println("The addition is: ", a+b);
+	}
+	
+	public void subtract(int a, int b){
+		System.out.println("The subtraction is: ", a-b);
+	}
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+
+public static void main(String[] args){
+
+Calculator c  = new Calculator();
+c1.add(10, 20);
+c1.subtract(20, 10);
+}
+
+
+
+
+}
